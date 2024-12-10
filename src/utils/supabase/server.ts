eb48696d9955e9c3d22243a4c1788d6773a04114
Dimitris <jimstef@outlook.com>
2024-12-11 00:11:32 +0200

    security

--- src/utils/supabase/server.ts
+++ src/utils/supabase/server.ts
@@ -7,7 +7,7 @@ export async function createClient() {
 
   return createServerClient<Database>(
     process.env.SUPABASE_URL!,
-    process.env.SUPABASE_ANON_KEY!,
+    process.env.SUPABASE_SERVICE_ROLE_KEY!,
     {
       cookies: {
         getAll() {
